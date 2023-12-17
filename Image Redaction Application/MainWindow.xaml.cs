de24c6b69680299dd6a2a19ebe5d0e2087687531
Casey Monroe <tun04492@temple.edu>
2023-12-17 12:23:18 -0500

    Added facial detection, improved eraseAll button

--- Image Redaction Application/MainWindow.xaml.cs
+++ Image Redaction Application/MainWindow.xaml.cs
@@ -9,34 +9,53 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
 using Formatting = Newtonsoft.Json.Formatting;
+//using OpenCvSharp;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Cuda;
+using System.Drawing;
+using OpenCvSharp;
+
+
 namespace Image_Redaction_Application
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         //global variables being initialized
         //isErasing starts off false, changes to true when erase is toggled
+        private Emgu.CV.CascadeClassifier _faceCascade;
+        private Emgu.CV.CascadeClassifier _faceCascadeSide;
         private bool isErasing = false;
         private SprayPaintData sprayPaintData = new SprayPaintData();
-        private double eraserSize = 10;
         //random variable and sizes for the spray paint brush
         private Random random = new Random();
-        private double opacity = 0.75;
-        private double brushDensity = 2;
         //variables for the image and file path
         private BitmapImage modifiedImage;
         private string filename;
-        //these are for the color of the spray paint
-        byte red = 0;
-        byte green = 0;
-        byte blue = 0;
+        private string fileNameExtension;
+        SolidColorBrush PaintColor = new SolidColorBrush();
 
         public MainWindow()
         {
             InitializeComponent();
-        }
+            try
+            {
+                _faceCascadeSide = new Emgu.CV.CascadeClassifier("haarcascade_profileface.xml");
+                _faceCascade = new Emgu.CV.CascadeClassifier("haarcascade_frontalface_default.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading frontal face cascade classifier: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                RedactFaces.IsEnabled = false;
 
+            }
+
+
+        }
+        //move these to a separate file
         [Serializable]
-        //spraypaintData class to hold ellipses from spray paint effect and store them
+        //spraraypaintData class to hold ellipses from spray paint effect and store them
         public class SprayPaintData
         {
             public List<SprayEllipse> Ellipses { get; set; } = new List<SprayEllipse>();
@@ -48,10 +67,14 @@ namespace Image_Redaction_Application
         {
             public double X { get; set; }
             public double Y { get; set; }
+            public double Width { get; set; }
+            public double Height { get; set; }
             public double Opacity { get; set; }
             public required SolidColorBrush EllipseColor { get; set; }
         }
 
+
+        //hardcoded values for bitmap dimensions and formats, consider making them configurable
         private void SaveModifiedImage(string filePath)
         {
             //visual will be used to combine the image with the canvas
@@ -60,7 +83,7 @@ namespace Image_Redaction_Application
             using (DrawingContext context = visual.RenderOpen())
             {
                 //creating copy of the image for spray paint to be drawn onto
-                context.DrawImage(modifiedImage, new Rect(new Size(Image.Width, Image.Height)));
+                context.DrawImage(modifiedImage, new System.Windows.Rect(new System.Windows.Size(Image.Width, Image.Height)));
 
                 //drawing the spray paint onto the image copy, 
                 foreach (SprayEllipse sprayEllipse in sprayPaintData.Ellipses)
@@ -69,7 +92,7 @@ namespace Image_Redaction_Application
                     //drawing the ellipses from spraypaint data onto the image
                     //these ellipses will be permanent, but the spray paint will be able to be edited
                     //by opening up the original image again
-                    context.DrawEllipse(brush, null, new Point(sprayEllipse.X, sprayEllipse.Y), 2, 2);
+                    context.DrawEllipse(brush, null, new System.Windows.Point(sprayEllipse.X, sprayEllipse.Y), 2, 2);
                 }
             }
 
@@ -115,7 +138,7 @@ namespace Image_Redaction_Application
             if (modifiedImage is BitmapImage bitmapImage && bitmapImage.UriSource != null)
             {
                 //constructing path for spray paint JSON
-                string sprayPaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Ellipses", $"{filename}-edited.json");
+                string sprayPaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, $"{filename}-edited.json");
                 SaveSprayPaint(sprayPaintFilePath);
                 //have user choose where to save a PNG copy of the image with spray paint applied
                 string modifiedImageFilePath = GetSaveFilePath("PNG Files (*.png)|*.png", "Select Image File");
@@ -129,6 +152,8 @@ namespace Image_Redaction_Application
             }
 
         }
+
+
 
         private string GetSaveFilePath(string filter, string title)
         {
@@ -156,7 +181,7 @@ namespace Image_Redaction_Application
                 {
                     foreach (var ellipseData in sprayPaintData.Ellipses)
                     {
-                        Ellipse smallEllipse = new Ellipse
+                        System.Windows.Shapes.Ellipse smallEllipse = new System.Windows.Shapes.Ellipse
                         {
                             Width = 3,
                             Height = 3,
@@ -175,6 +200,59 @@ namespace Image_Redaction_Application
             }
         }
 
+
+        private void SprayPaintFaces(Emgu.CV.Mat image, System.Drawing.Rectangle[] faces)
+        {
+            SolidColorBrush PaintColor = new SolidColorBrush();
+            PaintColor.Color = System.Windows.Media.Color.FromRgb((byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value);
+
+            foreach (var face in faces)
+            {
+                System.Windows.Shapes.Ellipse faceEllipse = new()
+                {
+                    Width = face.Width,
+                    Height = face.Height,
+                    Fill = PaintColor,
+                    Margin = new Thickness(face.X + (face.Width / 2) / 2, face.Y + (face.Height / 2) / 2, 0, 0),
+
+                    Opacity = 1.0
+                };
+
+                ImageControl.Children.Add(faceEllipse);
+                sprayPaintData.Ellipses.Add(new SprayEllipse
+                {
+                    X = face.X,
+                    Y = face.Y,
+                    Width = face.Width,
+                    Height = face.Height,
+                    Opacity = OpacitySlider.Value,
+                    EllipseColor = PaintColor
+                });
+            }
+        }
+        private void RedactFaces_Click(object sender, RoutedEventArgs e)
+        {
+            using (var image = CvInvoke.Imread(fileNameExtension))
+            {
+                if (image.IsEmpty)
+                {
+                    MessageBox.Show("Failed to load the image.");
+                    return;
+                }
+                CvInvoke.Resize(image, image, new System.Drawing.Size((int)Image.Width, (int)Image.Height));
+
+                using (var gray = new Emgu.CV.UMat())
+                {
+                    CvInvoke.CvtColor(image, gray, ColorConversion.Bgr2Gray);
+                    // Detect faces
+                    var faces = _faceCascade.DetectMultiScale(gray, 1.1, 10, new System.Drawing.Size(20, 20), System.Drawing.Size.Empty);
+                    var faces2 = _faceCascadeSide.DetectMultiScale(gray, 1.1, 10, new System.Drawing.Size(20, 20), System.Drawing.Size.Empty);
+                    SprayPaintFaces(image, faces);
+                    SprayPaintFaces(image, faces2);
+                    Debug.WriteLine("this finished");
+                }
+            }
+        }
         private void LoadImage_Click(object sender, RoutedEventArgs e)
         {
 
@@ -183,9 +261,9 @@ namespace Image_Redaction_Application
             if (modifiedImage is BitmapImage bitmapImage && bitmapImage.UriSource != null)
             {
                 //creating paths for the image file and ellipses JSON
-                string sprayPaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Ellipses", $"{filename}-edited.json");
+                string sprayPaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, $"{filename}-edited.json");
                 SaveSprayPaint(sprayPaintFilePath);
-                string ImagePaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Images", $"{filename}-painted.png");
+                string ImagePaintFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, $"{filename}-painted.png");
                 SaveModifiedImage(ImagePaintFilePath);
                 //resetting the canvas
                 Image.Source = null;
@@ -202,9 +280,12 @@ namespace Image_Redaction_Application
                 modifiedImage = new BitmapImage(new Uri(openFileDialog.FileName));
 
 
+
             }
+            fileNameExtension = System.IO.Path.GetFullPath(openFileDialog.FileName);
+            System.Diagnostics.Debug.WriteLine(fileNameExtension);
             filename = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName);
-            string EllipsesFile = System.IO.Path.Combine(Environment.CurrentDirectory, "Ellipses", $"{filename}-edited.json");
+            string EllipsesFile = System.IO.Path.Combine(Environment.CurrentDirectory, $"{filename}-edited.json");
             Debug.WriteLine(EllipsesFile + "is the path in loadimage_click");
             LoadSprayPaint(EllipsesFile);
             Debug.WriteLine("first file name: " + filename);
@@ -215,7 +296,7 @@ namespace Image_Redaction_Application
         private void ImageControl_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
-            Point position = e.GetPosition(ImageControl);
+            System.Windows.Point position = e.GetPosition(ImageControl);
             if (isErasing)
             {
                 EraseAtPosition(position);
@@ -230,7 +311,7 @@ namespace Image_Redaction_Application
 
         private void ImageControl_MouseMove(object sender, MouseEventArgs e)
         {
-            Point position = e.GetPosition(ImageControl);
+            System.Windows.Point position = e.GetPosition(ImageControl);
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 if (isErasing)
@@ -250,24 +331,27 @@ namespace Image_Redaction_Application
 
             //erase all button, it clears out the list of ellipses and removes all spray paint from the image canvas
             sprayPaintData.Ellipses.Clear();
-            //list of all ellipses on image, only removing them so the image does not disappear
-            var removalList = ImageControl.Children.OfType<Ellipse>().ToList();
 
-            foreach (var ellipse in removalList)
+            // Create a list to hold non-ellipse children that you want to keep
+            var BlankImageControl = ImageControl.Children.OfType<System.Windows.Controls.Image>().FirstOrDefault();
+            // Clear all children
+            ImageControl.Children.Clear();
+
+            // Add back the non-ellipse children
+            if (BlankImageControl != null)
             {
-                //removing each ellipse
-                ImageControl.Children.Remove(ellipse);
+                ImageControl.Children.Add(BlankImageControl);
             }
         }
         private HitTestResultBehavior HitTestCallback(HitTestResult result)
         {
             //checking if the result is an ellipse
-            if (result.VisualHit is Ellipse ellipse)
+            if (result.VisualHit is System.Windows.Shapes.Ellipse ellipse)
             {
                 //removing the ellipse from the image canvas
                 ImageControl.Children.Remove(ellipse);
                 //creating a point to get the point of the ellipse, then using it to remove the ellipse from the sprayPaintData list
-                Point ellipsePosition = new Point(Canvas.GetLeft(ellipse), Canvas.GetTop(ellipse));
+                System.Windows.Point ellipsePosition = new System.Windows.Point(Canvas.GetLeft(ellipse), Canvas.GetTop(ellipse));
                 //checking for nulls
                 if (sprayPaintData != null)
                 {
@@ -282,24 +366,24 @@ namespace Image_Redaction_Application
 
             return HitTestResultBehavior.Continue;
         }
-        private void EraseAtPosition(Point position)
+        private void EraseAtPosition(System.Windows.Point position)
         {
             //using half of the eraser to create a rectangle shape for the eraser
             //halferaser is used to minimize the number of divides
-            double halfEraser = eraserSize / 2;
-            Rect hitTestRect = new Rect(position.X - halfEraser, position.Y - halfEraser, eraserSize, eraserSize);
+            double halfEraser = EraserSizeSlider.Value / 2;
+            System.Windows.Rect hitTestRect = new System.Windows.Rect(position.X - halfEraser, position.Y - halfEraser, EraserSizeSlider.Value, EraserSizeSlider.Value);
             //using a hit test to remove ellipses in boundaries of eraser
             GeometryHitTestParameters hitTestParams = new GeometryHitTestParameters(new RectangleGeometry(hitTestRect));
             HitTestResultCallback callback = new HitTestResultCallback(HitTestCallback);
             VisualTreeHelper.HitTest(ImageControl, null, callback, hitTestParams);
         }
-        private void SprayPaint(Point position)
+        private void SprayPaint(System.Windows.Point position)
         {
 
-            SolidColorBrush PaintColor = new SolidColorBrush();
-            PaintColor.Color = Color.FromRgb(red, green, blue);
+            //SolidColorBrush PaintColor = new SolidColorBrush();
+            PaintColor.Color = System.Windows.Media.Color.FromRgb((byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value);
             //this is the number of small ellipses used for the spray paint effect
-            int numSmallEllipses = 15 + (int)brushDensity * 2;
+            int numSmallEllipses = 10 + (15 - (int)DensitySlider.Value);
             for (int i = 0; i < numSmallEllipses; i++)
             {
                 //math to randomly distribute smaller circle in the shape of a larger circle
@@ -308,17 +392,16 @@ namespace Image_Redaction_Application
                 double angle = (Math.PI * i) / numSmallEllipses + (random.NextDouble() * Math.PI);
                 //brushdensity is multiplied by 3 and subtracted from the maximum brush size to adjust the overall size of the distribution of the circles
                 //while it is smaller, the circles are more compact, and if it is bigger they are less compact
-
-                double xOffset = position.X + random.Next(1, (int)(33 - brushDensity * 3)) * Math.Cos(angle);
-                double yOffset = position.Y + random.Next(1, (int)(33 - brushDensity * 3)) * Math.Sin(angle);
+                double xOffset = position.X + random.Next(1, (int)(33 - DensitySlider.Value * 3)) * Math.Cos(angle);
+                double yOffset = position.Y + random.Next(1, (int)(33 - DensitySlider.Value * 3)) * Math.Sin(angle);
                 //creating ellipse that is a circle to be added to the canvas and spraypaintdata list
-                Ellipse smallEllipse = new()
+                System.Windows.Shapes.Ellipse smallEllipse = new()
                 {
-                    Width = 4,
-                    Height = 4,
+                    Width = 7,
+                    Height = 7,
                     Fill = PaintColor,
                     Margin = new Thickness(xOffset, yOffset, 0, 0),
-                    Opacity = opacity
+                    Opacity = OpacitySlider.Value
                 };
                 //adding this to the image canvas
                 ImageControl.Children.Add(smallEllipse);
@@ -327,46 +410,19 @@ namespace Image_Redaction_Application
                 {
                     X = xOffset,
                     Y = yOffset,
-                    Opacity = opacity,
+                    Opacity = OpacitySlider.Value,
                     EllipseColor = PaintColor
                 });
             }
         }
 
-
         //Constructors for different buttons
-        private void DensitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            brushDensity = DensitySlider.Value;
-        }
-
-        private void OpacitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            opacity = OpacitySlider.Value;
-        }
-
-        private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            red = (byte)RedSlider.Value;
-
-        }
-
-        private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            green = (byte)GreenSlider.Value;
-
-        }
-
-        private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            blue = (byte)BlueSlider.Value;
-
-        }
-        private void EraserSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            // Update the eraser size when the slider value changes
-            eraserSize = EraserSizeSlider.Value;
-        }
+        private void DensitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
+        private void OpacitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
+        private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
+        private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
+        private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
+        private void EraserSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { }
         private void EraseButton_Click(object sender, RoutedEventArgs e)
         {
             //erase is a toggle button, click it to change from spray paint to erase
